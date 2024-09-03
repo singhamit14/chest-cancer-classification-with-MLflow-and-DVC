46449a352d528b4a03eaeddad958fcf6e038a1f2
singhamit14 <singhamit.iot@gmail.com>
2024-09-03 20:41:19 +0530

    added data ingestion

---
+++ main.py
@@ -0,0 +1,14 @@
+from cnnClassifier import logger
+from cnnClassifier.pipeline.data_ingestion_stage_01 import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
