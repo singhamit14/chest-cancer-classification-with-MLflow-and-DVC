9dbfc9def973b7b3a2fd0a225f312acd1b3700ef
singhamit14 <singhamit.iot@gmail.com>
2024-09-05 19:50:25 +0530

    model training completed

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.data_ingestion_stage_01 import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.prepare_base_model_02 import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.model_trainer_03 import ModelTrainingPipeline
+
+
+
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -22,6 +26,22 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
