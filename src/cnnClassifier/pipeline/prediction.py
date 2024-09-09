16d25e7470a27c1cebbed7b03026b74cff70c153
singhamit14 <singhamit.iot@gmail.com>
2024-09-09 20:37:21 +0530

    model & prediction pipeline creted

---
+++ src/cnnClassifier/pipeline/prediction.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+from tensorflow.python.keras.models import load_model
+from tensorflow.python.keras.preprocessing import image
+
+
+
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename = filename
+
+
+    def predict(self):
+        # load model
+        model = load_model(os.path.join("model","model.h5"))
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (224, 224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis = 1)
+
+        if result[0] == 1:
+            prediction = 'Normal'
+            return [{"image": prediction}]
+        else:
+            prediction = 'Adenocarcinoma Cancer'
+            return [{"image": prediction}]
+        
